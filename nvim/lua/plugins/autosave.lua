cadceec704fff2e6109fe611e126c140c42c73e0
quangbach <167604957+quangbach27@users.noreply.github.com>
2026-08-11 16:01:46 +0700

    Add autosave and go plugin

---
+++ nvim/lua/plugins/autosave.lua
@@ -0,0 +1,8 @@
+return {
+	"okuuva/auto-save.nvim",
+	version = "^1.0.0",
+	cmd = "ASToggle",
+	enabled = true,
+	lazy = false,
+	opts = { debounce_delay = 2000 },
+}
--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -14,7 +14,7 @@ return {
 				nerd_font_variant = "mono",
 			},
 
-			completion = { documentation = { auto_show = false } },
+			completion = { documentation = { auto_show = true } },
 
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer" },
